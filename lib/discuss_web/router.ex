3bf706e368ad73d5df8aa8d5c33a92eee048d6b5
Amr Elsekilly <github@amrsekilly.com>
2018-10-27 15:58:00 +0200

    Added a rout to create a new topics

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,6 +17,7 @@ defmodule DiscussWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/new/topic", TopicConteroller, :new
   end
 
   # Other scopes may use custom stacks.
