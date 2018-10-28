012f1f444000d6d84070b1d3cdc346d029d02ed1
Amr Elsekilly <github@amrsekilly.com>
2018-10-28 19:59:43 +0200

    Created a schema for the topics

---
+++ lib/discuss/topics/topic.ex
@@ -0,0 +1,9 @@
+defmodule Discuss.Topics.Topic do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  schema "topics" do
+    field :title, :string
+  end
+end
--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,9 +2,6 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   def new(conn, params) do
-    IO.puts "----------"
-    IO.inspect conn
-    IO.puts "----------"
-    IO.inspect params
+    conn
   end
 end
