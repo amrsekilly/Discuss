726a82261f90b312a2618a8f2851aa8eb349178c
Amr Elsekilly <github@amrsekilly.com>
2018-10-30 02:51:53 +0200

    Testing the changeset with a sample topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,8 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  alias Discuss.Topics.Topic
 
   def new(conn, params) do
-    conn
+    changeset = Topic.changeset(%Topic{}, %{title: "Testing with a topic"})
   end
 end
