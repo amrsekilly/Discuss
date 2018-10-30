ffb90bf57afa92c0b78bfbd2d00144442bd021d7
Amr Elsekilly <github@amrsekilly.com>
2018-10-30 03:41:06 +0200

    Ignored the params, cuz I won't use them

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,13 +2,14 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topics.Topic
 
-  def new(conn, params) do
-    changeset = Topic.changeset(%Topic{}, %{title: "Testing with a topic"})
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do
+    IO.inspect conn
     IO.inspect topic
   end
 
