4a0c957507542ede6ac04cb1d8ef0a161cb1d15f
Amr Elsekilly <github@amrsekilly.com>
2018-10-31 13:56:22 +0200

    Changed the scope of the router

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -13,12 +13,12 @@ defmodule DiscussWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", DiscussWeb do
+  scope "/topics", DiscussWeb do
     pipe_through :browser # Use the default browser stack
 
     get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    get "/new", TopicController, :new
+    post "/", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
