11b1e1a665ebc70742a325ccbba80875e9cc4767
Amr Elsekilly <github@amrsekilly.com>
2018-10-31 14:03:43 +0200

    Completed the flow of saving a new topic and redirecting the user to see a list of the saved topics

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -17,7 +17,10 @@ defmodule DiscussWeb.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect post
+      {:ok, post} -> 
+        conn
+        |> put_flash(:info, "The topic was created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> render conn, "new.html", changeset: changeset   
     end
     
