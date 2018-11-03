94bdeb5b573b8e4a95819f5a91207dddd6f20fc0
Amr Elsekilly <github@amrsekilly.com>
2018-11-03 23:26:40 +0200

    Added a plug to verify that the user is logged in

---
+++ lib/discuss_web/controllers/plugs/require_auth.ex
@@ -0,0 +1,19 @@
+defmodule Discuss.Plugs.RequireAuth do
+  import Plug.Conn
+  import Phoenix.Controller
+  alias Discuss.Router.Helpers
+
+  def init(_) do end
+
+  def call(conn, _) do
+    cond do
+      conn.assigns[:user] -> conn
+      true -> 
+        conn
+        |> put_flash(:error, "You must be signed in!")
+        |> redirect(to: Helpers.topic_path(conn, :index))
+        |> halt
+    end
+  end
+
+end
