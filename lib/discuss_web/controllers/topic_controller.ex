cabe6df706f9f1f438f34204e534cd773f6b7728
Amr Elsekilly <github@amrsekilly.com>
2018-11-04 01:52:18 +0200

    wrote a plug to check for the topic owner to allow them to edit or delete the topics they own

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -5,6 +5,8 @@ defmodule DiscussWeb.TopicController do
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
 
+  plug :check_topic_owner when action in [:edit, :update, :delete]
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -32,7 +34,7 @@ defmodule DiscussWeb.TopicController do
 
   end
 
-  def delete(conn, %{"id" => id} = params) do
+  def delete(conn, %{"id" => id}) do
     Repo.get!(Topic, id) |> Repo.delete! 
       conn
       |> put_flash(:info, "Topic was deleted successfully!!")
@@ -58,6 +60,19 @@ defmodule DiscussWeb.TopicController do
       {:error, changeset} -> render conn, "new.html", changeset: changeset   
     end
     
+  end
+
+  defp check_topic_owner(%{params: %{"id" => topic_id}} = conn, _) do
+  
+    cond do 
+      Repo.get(Topic, topic_id).user_id == conn.assigns.user.id -> 
+        conn
+    true -> 
+      conn
+      |> put_flash(:error, "You can't edit that!")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt
+    end
   end
 
 end
