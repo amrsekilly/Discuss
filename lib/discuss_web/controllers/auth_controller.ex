e6cc76119bff4d0b58de8091a0c47d876799b6dd
Amr Elsekilly <github@amrsekilly.com>
2018-11-03 18:13:11 +0200

    cleaned up the struct

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -3,13 +3,14 @@ defmodule DiscussWeb.AuthController do
   plug Ueberauth
   
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
-    user_params = %{token: auth.credentials.token, email: auth.info.email, username: auth.info.name, provider: provider}
+    user_params = %{
+      token: auth.credentials.token, 
+      email: auth.info.email, 
+      username: auth.info.name,
+      provider: provider
+    }
 
-    IO.inspect "----------------------"
     IO.inspect user_params
-    IO.inspect "----------------------"
-    IO.inspect params
-    IO.inspect "----------------------"
 
   end
 end
