8b960025e830e72ca4cbb05827f77a451a0faacf
Amr Elsekilly <github@amrsekilly.com>
2018-11-03 18:09:39 +0200

    Got the info that I need from the request

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -2,9 +2,14 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
   
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    user_params = %{token: auth.credentials.token, email: auth.info.email, username: auth.info.name}
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, username: auth.info.name, provider: provider}
+
+    IO.inspect "----------------------"
     IO.inspect user_params
+    IO.inspect "----------------------"
     IO.inspect params
+    IO.inspect "----------------------"
+
   end
 end
