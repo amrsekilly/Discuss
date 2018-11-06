bb3fb1e1d60e8522804e503d76723c307d5e59fd
Amr Elsekilly <github@amrsekilly.com>
2018-11-06 18:19:53 +0200

    sent a msg from js to the ph.channel

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -2,13 +2,16 @@ defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
 
   def join(name, _, socket) do
-    IO.puts "-----------"
-    IO.puts name
-    IO.puts "-----------"
-    {:ok, %{}, socket}
+    {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in do
+  def handle_in(name, message, socket) do
+    IO.puts "---------------"
+    IO.puts name
+    IO.puts "---------------"
+    IO.inspect message 
+    IO.puts "---------------"
     
+    {:reply, :ok, socket}
   end
 end
