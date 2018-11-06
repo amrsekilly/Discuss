ff71db82196d0e692e3ab1332bb46f4e05c23e15
Amr Elsekilly <github@amrsekilly.com>
2018-11-06 20:46:42 +0200

    Sent a message from the client to the server

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,17 +1,17 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
+  alias Discuss.Repo
+  alias Discuss.Topics.Topic
 
-  def join(name, _, socket) do
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get!(Topic, topic_id)
+    
+    {:ok, %{}, socket}
   end
 
   def handle_in(name, message, socket) do
-    IO.puts "---------------"
-    IO.puts name
-    IO.puts "---------------"
-    IO.inspect message 
-    IO.puts "---------------"
-    
+    IO.inspect message
     {:reply, :ok, socket}
   end
 end
